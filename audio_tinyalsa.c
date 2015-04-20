01e6a73431169e2bd76e24042d824e28925c9b05
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2015-04-20 15:25:16 +0200

    Pimp tinyalsa driver

--- audio_tinyalsa.c
+++ audio_tinyalsa.c
@@ -28,6 +28,7 @@
 #include <unistd.h>
 #include <memory.h>
 #include <tinyalsa/asoundlib.h>
+#include <sound/asound.h>
 #include "common.h"
 #include "audio.h"
 
@@ -35,6 +36,7 @@ static void help(void);
 static int init(int argc, char **argv);
 static void deinit(void);
 static void start(int sample_rate);
+static uint32_t delay(void);
 static void play(short buf[], int samples);
 static void stop(void);
 static void volume(double vol);
@@ -55,53 +57,35 @@ audio_output audio_tinyalsa = {
 
 static struct pcm *pcm = NULL;
 
-static struct mixer *alsa_mix_handle = NULL;
-static struct mixer_ctl *volume_ctl = NULL;
-
-static char *alsa_out_dev = "default";
-static char *alsa_mix_dev = NULL;
-static char *alsa_mix_ctrl = "Master";
-static int alsa_mix_index = 0;
+static int autodetect = 0;
+#ifdef __ANDROID__
+static int mmap = 0;
+#else
+static int mmap = 1;
+#endif
 static int card = 0;
 static int device = 0;
 static int rate = 44100;
 
 static void help(void) {
-    printf("    -C alsa-card        set the card id [0*|...]\n"
+    printf("    -A                  autodetect alsa card/device\n"
+#ifdef __ANDROID__
+           "    -M mmap             set mmap enabled/disabled [0|1*]\n"
+#else
+           "    -M mmap             set mmap enabled/disabled [0*|1]\n"
+#endif
+
+           "    -C alsa-card        set the card id [0*|...]\n"
            "    -d alsa-device      set the device id [0*|...]\n"
-		   "    -r sample-rate      set the sample rate[44100*|...]\n"
-           //"    -i mixer-index      set the mixer index [0*|...]\n"
+		   //"    -r sample-rate      set the sample rate[44100*|...]\n"
            "    *) default option\n"
           );
 }
 
-static int init(int argc, char **argv) {
-    int hardware_mixer = 0;
-
-    optind = 1; // optind=0 is equivalent to optind=1 plus special behaviour
-    argv--;     // so we shift the arguments to satisfy getopt()
-    argc++;
-    // some platforms apparently require optreset = 1; - which?
-    int opt;
-    while ((opt = getopt(argc, argv, "C:d:r:")) > 0) {
-        switch (opt) {
-            case 'C':
-                card = strtol(optarg, NULL, 10);
-                break;
-            case 'd':
-                device = strtol(optarg, NULL, 10);
-                break;
-			case 'r':
-                rate = strtol(optarg, NULL, 10);
-                break;
-            default:
-                help();
-                die("Invalid audio option -%c specified", opt);
-        }
-    }
-
-    if (optind < argc)
-        die("Invalid audio argument: %s", argv[optind]);
+static struct pcm *open_pcm(int card, int device)
+{
+    struct pcm_params *p;
+    int flags = PCM_OUT | (mmap ? PCM_MMAP : 0);
 
 	struct pcm_config config = {
 		.channels = 2,
@@ -112,32 +96,112 @@ static int init(int argc, char **argv) {
 		.start_threshold = 512 * 1
 	};
 
-	pcm = pcm_open(card, device, PCM_OUT, &config);
-	if (!pcm || !pcm_is_ready(pcm))
-		die("Failed to open PCM device %d:%d: %s", card, device, pcm_get_error(pcm));
+    p = pcm_params_get(card, device, PCM_OUT);
+    if (p) {
+        config.period_size = pcm_params_get_min(p, PCM_PARAM_PERIOD_SIZE);
+        config.period_count = pcm_params_get_min(p, PCM_PARAM_PERIODS);
+        config.start_threshold = config.period_size * 1;
 
-	alsa_mix_handle = mixer_open(card);
-	if (alsa_mix_handle) {
-		volume_ctl = mixer_get_ctl_by_name(alsa_mix_handle, "Volume");
-	} else {
-		warn("No mixer available!");
-	}
+        pcm_params_free(p);
+    }
+
+    return pcm_open(card, device, flags, &config);
+}
+
+static init_pcm() {
+
+    char path[128];
+
+    if (autodetect) {
+        int found = 0;
+        for (; card != 127; ++card) {
+            for (; device != 255; ++device) {
+                snprintf(path, sizeof(path), 
+                        "/dev/snd/pcmC%dD%dp", card, device);
+
+                if (access(path, F_OK) == 0) {
+                    pcm = open_pcm(card, device);
+                    if (pcm && pcm_is_ready(pcm)) {
+                        inform("Using PCM %d:%d", card, device);
+                        found = 1;
+                        break;
+                    } 
+                } else {
+                    //break;
+                }
+            }
+
+            if (found /*|| device == 0*/) {
+                break;
+            }
+        }
+
+        if (!found) {
+            die("Autodetection of PCM device failed");
+        }
+    } else {
+        pcm = open_pcm(card, device);
+        if (!pcm || !pcm_is_ready(pcm))
+            die("PCM device %d:%d: %s", card, device, pcm_get_error(pcm));
+    }
+
+	debug(2, "PCM device %d:%d intialized", card, device);
+}
+
+
+static int init(int argc, char **argv) {
+    int hardware_mixer = 0;
+
+    optind = 1; // optind=0 is equivalent to optind=1 plus special behaviour
+    argv--;     // so we shift the arguments to satisfy getopt()
+    argc++;
+    // some platforms apparently require optreset = 1; - which?
+    int opt;
+    while ((opt = getopt(argc, argv, "AM:D:C:d:")) > 0) {
+        switch (opt) {
+            case 'A':
+                autodetect = 1;
+                break;
+            case 'M':
+                mmap = optarg[0] == '1' ? 1 : 0;
+                break;
+            case 'C':
+                card = strtol(optarg, NULL, 10);
+                break;
+            case 'd':
+                device = strtol(optarg, NULL, 10);
+                break;
+			//case 'r':
+            //    rate = strtol(optarg, NULL, 10);
+            //    break;
+            default:
+                help();
+                die("Invalid audio option -%c specified", opt);
+        }
+    }
+
+    if (optind < argc)
+        die("Invalid audio argument: %s", argv[optind]);
+
+    init_pcm();
+
+    if(pcm_write(pcm, NULL, 0) < 0)
+        die("First write failed: %s", pcm_get_error(pcm));
 
 	return 0;
 }
 
 static void deinit(void) {
-	if (alsa_mix_handle)
-		mixer_close(alsa_mix_handle);
-
     stop();
 }
 
 static void start(int sample_rate) {
+	if (rate != 44100)
+		die("Unexpected sample rate");
 }
 
 static void play(short buf[], int samples) {
-	int err = pcm_write(pcm, buf, samples);
+	int err = pcm_write(pcm, (char *)buf, samples * 4);
 	if (err < 0)
         die("Failed to write to PCM device: %s", pcm_get_error(pcm));
 }
